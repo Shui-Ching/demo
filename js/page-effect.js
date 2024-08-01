b5d3681b0e4f3b1215b37d1fb61f2de29f07374f
andrewcheng@systex.com <Systex1986/10/24>
2024-08-01 21:10:41 +0800

    毛寶
    demo

--- js/page-effect.js
+++ js/page-effect.js
@@ -1,44 +1,7 @@
 
 
 
-// <!-- 調整-圖片放大 -->
 
-    $(document).ready(function() {
-
-      $('.image-popup-vertical-fit').magnificPopup({
-        type: 'image',
-        closeOnContentClick: true,
-        mainClass: 'mfp-img-mobile',
-        image: {
-          verticalFit: true
-        }
-        
-      });
-
-      $('.image-popup-fit-width').magnificPopup({
-        type: 'image',
-        closeOnContentClick: true,
-        image: {
-          verticalFit: false
-        }
-      });
-
-      $('.image-popup-no-margins').magnificPopup({
-        type: 'image',
-        closeOnContentClick: true,
-        closeBtnInside: false,
-        fixedContentPos: true,
-        mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
-        image: {
-          verticalFit: true
-        },
-        zoom: {
-          enabled: true,
-          duration: 300 // don't foget to change the duration also in CSS
-        }
-      });
-
-    });
 
 //  <!-- Initialize Swiper -->
     //小圖
@@ -68,9 +31,60 @@
 //----------進場
 AOS.init();
 
-//----------內容區向下滑動增加class
+
+
+
+
+
 $(document).ready(function() {
-  let lastScrollTop = 0;
+// <!-- 調整-圖片放大 -->
+  $('.image-popup-vertical-fit').magnificPopup({
+    type: 'image',
+    closeOnContentClick: true,
+    mainClass: 'mfp-img-mobile',
+    image: {
+      verticalFit: true
+    }
+    
+  });
+
+  $('.image-popup-fit-width').magnificPopup({
+    type: 'image',
+    closeOnContentClick: true,
+    image: {
+      verticalFit: false
+    }
+  });
+
+  $('.image-popup-no-margins').magnificPopup({
+    type: 'image',
+    closeOnContentClick: true,
+    closeBtnInside: false,
+    fixedContentPos: true,
+    mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
+    image: {
+      verticalFit: true
+    },
+    zoom: {
+      enabled: true,
+      duration: 300 // don't foget to change the duration also in CSS
+    }
+  });
+
+//lightbox 影片
+  $('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
+    disableOn: 700,
+    type: 'iframe',
+    mainClass: 'mfp-fade',
+    removalDelay: 160,
+    preloader: false,
+
+    fixedContentPos: false
+  });
+
+//----------內容區向下滑動增加class
+
+ let lastScrollTop = 0;
   let scrollDownDistance = 0;
 
   $(window).scroll(function() {
@@ -90,4 +104,44 @@ $(document).ready(function() {
 
     lastScrollTop = currentScrollTop;
   });
+
+
 });
+
+//lightbox 影片
+// $(document).ready(function() {
+//   $('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
+//     disableOn: 700,
+//     type: 'iframe',
+//     mainClass: 'mfp-fade',
+//     removalDelay: 160,
+//     preloader: false,
+
+//     fixedContentPos: false
+//   });
+// });
+
+
+//----------內容區向下滑動增加class
+// $(document).ready(function() {
+//   let lastScrollTop = 0;
+//   let scrollDownDistance = 0;
+
+//   $(window).scroll(function() {
+//     let currentScrollTop = $(this).scrollTop();
+    
+//     if (currentScrollTop > lastScrollTop) {
+//       scrollDownDistance += currentScrollTop - lastScrollTop;
+//       if (scrollDownDistance >= 300) {
+//         $('.mainContent').addClass('scrolled-down');
+//       }
+//     } else {
+//       scrollDownDistance -= lastScrollTop - currentScrollTop;
+//       if (scrollDownDistance <= 400) {
+//         $('.mainContent').removeClass('scrolled-down');
+//       }
+//     }
+
+//     lastScrollTop = currentScrollTop;
+//   });
+// });
